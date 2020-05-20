dc241da3c9368e95a251c8db030808ba543b79d9
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-05-19 23:13:53 -0300

    Configured Babel Absolute Path

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import configureStore from './src/store/ducks/store/configureStore'
 
 import { StatusBar, View} from 'react-native';
 
+import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-
 import Router from './src/navigation/navigations'
 
 const store=configureStore()
@@ -17,10 +17,14 @@ class App extends React.Component{
 
         async componentDidMount() {
             await Font.loadAsync({
-                'big_noodle_titling': {
-                    uri: require('./src/assets/fonts/big_noodle_titling.ttf'),
+                'steelfish-regular': {
+                    uri: require('assets/fonts/steelfish-rg.ttf'),
             },
-            });
+        });
+            await Asset.loadAsync([
+               require('assets/images/burguers/burguerChiken.png')
+           ] ),
+
         
             this.setState({ fontLoaded: true });
           }
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+            '.png'
+          ],
+          root: ['./src']
+        }
+      ]
+    ]
   };
 };
